ac2be5e51883c3cb6902890671da05b739621e78
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-25 17:13:47 -0300

    style: removing blank line's whitespaces

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,12 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-    
+
     users = 0
     for login, logout in permanence_period:
         if type(login) is not int or type(logout) is not int:
             return None
-        
+
         if login <= target_time <= logout:
             users += 1
 
