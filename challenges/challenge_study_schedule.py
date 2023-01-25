fce17527251c13cb630d52914d701f1216ee967d
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-25 17:06:24 -0300

    feat: req 01 - creates a function that returns the quantity of students attending the platform on a given time range

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui.."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+    
+    users = 0
+    for login, logout in permanence_period:
+        if type(login) is not int or type(logout) is not int:
+            return None
+        
+        if login <= target_time <= logout:
+            users += 1
+
+    return users
