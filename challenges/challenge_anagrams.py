b8cbe50ee967e763c3cdfe2675e5c2c19481feec
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-01-25 19:58:59 -0300

    feat: req 04 - creates a function that compares two strings and return'em sorted and if they are or are not anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,29 @@
+def sort_it(string):
+    listed = list(string)
+
+    for i in range(len(listed)):
+        for j in range(i + 1, len(listed)):
+            if listed[i] > listed[j]:
+                listed[i], listed[j] = listed[j], listed[i]
+
+    return "".join(listed)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    status = False
+    if not first_string or len(first_string) < 1:
+        return ["", second_string, status]
+
+    lowered_1st = first_string.lower()
+
+    if not second_string or len(second_string) < 1:
+        return [lowered_1st, "", status]
+
+    lowered_2nd = second_string.lower()
+
+    sorted_1st = sort_it(lowered_1st)
+    sorted_2nd = sort_it(lowered_2nd)
+
+    status = sorted_1st == sorted_2nd
+
+    return (sorted_1st, sorted_2nd, status)
